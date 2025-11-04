80a34eace97279d4fd73d38cfb9dab4a69c3169a
pjcalkins2229 <your-email@example.com>
2025-11-03 18:04:09 -0600

    Fix rbenv path configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,5 @@ set :deploy_to, '/home/deployer/sites/prototypes'
 set :deploy_env, "production"
 set :rails_env, "production"
 set :branch, "main"
-set :rbenv_path, "/home/deployer/.rbenv/bin/rbenv"
+set :rbenv_path, "/home/deployer/.rbenv"
+set :rbenv_ruby, '3.3.6'
